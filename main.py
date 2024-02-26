f7ad7894370d92e9299cd8ac761ebd192a7c42d8
Ahmethan Özcan <85957900+AhmethanOzcan@users.noreply.github.com>
2024-02-26 19:38:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ import get_application
 import gpt_creator
 import url_collector
 import mail_sender
-import re
 from dotenv import load_dotenv
+from email_validator import validate_email, EmailNotValidError
+
+def check(email):
+    try:
+      # validate and get info
+        v = validate_email(email) 
+        # replace with normalized form
+        email = v["email"]  
+        return email
+    except EmailNotValidError as e:
+        # email is not valid, exception message is human-readable
+        print("Problem:",str(e),"\tEmail:",email)
+        return None
 
 def main(pdf_route):
     load_dotenv()
@@ -15,15 +27,15 @@ def main(pdf_route):
     reference_file = open("references.txt", "a+")
     reference_file.seek(0)
     reference_list = reference_file.readlines()
-    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
 
     for i in url:
         job_dict = get_application.get_from_url(i)
         # job_info_list.append(job_dict)
-        if job_dict["reference_number"]+"\n" not in reference_list and re.fullmatch(regex, job_dict["email"]):
-            job_info_list.append(job_dict)
-        else:
-            print("Already sent mail to "+job_dict["reference_number"])
+        if check(job_dict["email"]) != None:
+            if job_dict["reference_number"]+"\n" not in reference_list:
+                job_info_list.append(job_dict)
+            else:
+                print("Already sent mail to "+job_dict["reference_number"])
     # dicte= {
     #     "firm_name"          : "firm_name",
     #     "job_title"          : "job_title",
